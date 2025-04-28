ea38f5be311c28d38b7226fb7eab9d5c40d1b79d
1xifeng <3091069683@qq.com>
2025-04-28 09:12:32 +0800

    制作主题功能中，目前bug有点多

--- app/src/main/java/com/swu/myapplication/MainActivity.kt
+++ app/src/main/java/com/swu/myapplication/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.swu.myapplication
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
@@ -9,13 +11,18 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.swu.myapplication.databinding.ActivityMainBinding
+import com.swu.myapplication.utils.ThemeManager
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var navController: NavController
+    private lateinit var themeManager: ThemeManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        // 在界面创建前应用主题
+        themeManager = ThemeManager.getInstance(this)
+        
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -31,6 +38,25 @@ class MainActivity : AppCompatActivity() {
         
         // 隐藏ActionBar
         supportActionBar?.hide()
+    }
+    
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        
+        // 如果设置了跟随系统且系统深色模式变化
+        if (themeManager.isFollowSystem()) {
+            val currentNightMode = newConfig.uiMode and Configuration.UI_MODE_NIGHT_MASK
+            when (currentNightMode) {
+                Configuration.UI_MODE_NIGHT_YES -> {
+                    // 系统切换到夜间模式
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                }
+                Configuration.UI_MODE_NIGHT_NO -> {
+                    // 系统切换到浅色模式
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                }
+            }
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
